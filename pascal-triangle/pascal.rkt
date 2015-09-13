70a684eb85c339092ada480ff6a24e1ef6e98ade
Scott Windsor <swindsor@gmail.com>
2015-09-13 16:43:17 -0700

    Full racket solution

--- pascal-triangle/pascal.rkt
+++ pascal-triangle/pascal.rkt
@@ -1,21 +1,31 @@
-#lang racket/base
+#lang racket
 
 (require rackunit)
+
+(define (sums l)
+  (cond [(null? l) '()]
+        [(null? (cdr l)) l]
+        [else
+          (cons (+ (car l) (cadr l))
+                (sums (cdr l)))]))
+
+(check-equal? (sums '(1)) '(1))
+(check-equal? (sums '(1 1)) '(2 1))
+(check-equal? (sums '(1 2 1)) '(3 3 1))
+(check-equal? (sums '(1 3 3 1)) '(4 6 4 1))
 
 (define (pascal-triangle n)
   (case n
     [(1) '((1))]
-    [(2) (append (pascal-triangle (sub1 n)) '((1 1)))]
     [else
       (letrec ([t (pascal-triangle (sub1 n))]
-               [tl (cadr t)])
+               [tl (last t)])
         (append
           t
-          (list
-            (cons (car tl)
-              (cons (+ (car tl) (cadr tl))
-                    (cdr tl))))))]))
+          (list (cons 1 (sums tl)))))]))
 
 (check-equal? (pascal-triangle 1) '((1)))
 (check-equal? (pascal-triangle 2) '((1) (1 1)))
 (check-equal? (pascal-triangle 3) '((1) (1 1) (1 2 1)))
+(check-equal? (pascal-triangle 4) '((1) (1 1) (1 2 1) (1 3 3 1)))
+(check-equal? (pascal-triangle 5) '((1) (1 1) (1 2 1) (1 3 3 1) (1 4 6 4 1)))
